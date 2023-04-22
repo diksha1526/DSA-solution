dea18efc8b3bf1c77189de1c0a8b5fa4a50b749e
diksha1526 <119476889+diksha1526@users.noreply.github.com>
2023-04-22 18:01:57 +0530

    Delete 1. Two Sum.py

--- 1. Two Sum.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for i in range(len(nums)):
-            r=nums[i+1:]
-            if (target - nums[i]) in r:
-                return [i,i+1+r.index(target-nums[i])]
-            
